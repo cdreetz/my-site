e4ecd7375adb654fb9bac6587db646771ee84956
Christian <cdreetz@gmail.com>
2024-09-08 00:05:15 -0500

    idk anymore

--- infra/main.tf
+++ infra/main.tf
@@ -63,6 +63,29 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn        = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket_website_configuration.website_config.website_endpoint
