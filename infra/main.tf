29a8fda7c64d328b2326008125ff31544cd173ad
Christian <cdreetz@gmail.com>
2024-09-07 23:32:33 -0500

    acm permissions

--- infra/main.tf
+++ infra/main.tf
@@ -8,12 +8,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "website_bucket" {
+data "aws_s3_bucket" "website_bucket" {
   bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
-  bucket = aws_s3_bucket.website_bucket.id
+  bucket = data.aws_s3_bucket.website_bucket.id
 
   index_document {
     suffix = "index.html"
@@ -25,7 +25,7 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
 }
 
 resource "aws_s3_bucket_public_access_block" "website_public_access" {
-  bucket = aws_s3_bucket.website_bucket.id
+  bucket = data.aws_s3_bucket.website_bucket.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -34,7 +34,7 @@ resource "aws_s3_bucket_public_access_block" "website_public_access" {
 }
 
 resource "aws_s3_bucket_policy" "website_bucket_policy" {
-  bucket = aws_s3_bucket.website_bucket.id
+  bucket = data.aws_s3_bucket.website_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
